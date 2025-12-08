991eb30b36ccb4e7dbfde21524ced0d26fd643ea
Abdul <ah303130gmail.com>
2025-12-08 14:20:38 +0530

    Fix server.js for TiDB prefix

--- db.js
+++ db.js
@@ -1,3 +1,26 @@
+// import mysql from 'mysql2';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Database connection failed:', err);
+//   } else {
+//     console.log('Connected to MySQL');
+//   }
+// });
+
+// export default db;
+
+
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -7,14 +30,18 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Database connection failed:', err);
+    console.error("Database connection failed:", err);
   } else {
-    console.log('Connected to MySQL');
+    console.log("Connected to TiDB Cloud MySQL");
   }
 });
 
